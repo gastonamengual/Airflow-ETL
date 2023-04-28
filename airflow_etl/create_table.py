ab3dd6ce321b95fcfb993cb048e4e41c9477f235
Gaston <gastonamengual@icloud.com>
2023-04-28 11:18:52 -0300

    initial commit

---
+++ airflow_etl/create_table.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+# create a database connection and cursor
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+
+# create a table to store our data
+c.execute('''
+          CREATE TABLE sales (
+              id INTEGER PRIMARY KEY,
+              date DATE,
+              product TEXT,
+              price FLOAT
+          )
+          ''')
+
+# add some data to the table
+c.execute('''
+          INSERT INTO sales (date, product, price)
+          VALUES
+              ('2023-01-01', 'Product A', 100.0),
+              ('2023-01-02', 'Product B', 150.0),
+              ('2023-01-03', 'Product A', 200.0),
+              ('2023-01-04', 'Product C', 75.0),
+              ('2023-01-05', 'Product B', 120.0),
+              ('2023-01-06', 'Product C', 90.0),
+              ('2023-01-07', 'Product A', 180.0),
+              ('2023-01-08', 'Product B', 130.0),
+              ('2023-01-09', 'Product C', 80.0),
+              ('2023-01-10', 'Product A', 190.0)
+          ''')
+
+# commit changes and close connection
+conn.commit()
+conn.close()
---
+++ airflow_etl/extract.py
@@ -0,0 +1,7 @@
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+
+sqllite_operator = SqliteOperator(
+    task_id='read_table',
+    sqlite_conn_id='sqlite_default',
+    sql='SELECT * FROM my_table',
+)
